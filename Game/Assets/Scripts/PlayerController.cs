7945d24627205cb932cfd173d2912b925c17d926
TheSwaGiraffe <tojaxon@gmail.com>
2025-12-21 11:45:11 -0800

    Added rigidbody player movement

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] float MoveSpeed;
+    [SerializeField] float JumpPower;
+    [Header("References")]
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform sprite;
+    [SerializeField] BoxCollider2D groundedHitbox;
+    [SerializeField] LayerMask GroundLayer;
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        bool jumping = Input.GetKey("up") || Input.GetKey("w") || Input.GetKey("space");
+
+        rb.linearVelocity = new Vector2(xInput * MoveSpeed, rb.linearVelocity.y);
+        if (jumping && groundedHitbox.IsTouchingLayers(GroundLayer)){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpPower);
+        }
+        updateSpriteVisual();
+        if(rb.linearVelocity.magnitude == 0){
+            transform.position = sprite.position;
+        }
+    }
+    void updateSpriteVisual()
+    {
+        //round position to nearest 8th so the sprite is pixel perfect
+        sprite.position = transform.position;
+        float newX = Mathf.Round(sprite.position.x*8)/8;
+        float newY = Mathf.Round(sprite.position.y*8)/8;
+        float newZ = sprite.position.z;
+        sprite.position = new Vector3(newX, newY, newZ);
+    }
+}
