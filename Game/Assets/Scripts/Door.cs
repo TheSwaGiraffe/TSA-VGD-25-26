0841dd3398b0054b4baf0ac262df599f566ce34e
TheSwaGiraffe <tojaxon@gmail.com>
2026-01-20 11:35:02 -0600

    started on level 1-2

--- Game/Assets/Scripts/Door.cs
+++ Game/Assets/Scripts/Door.cs
@@ -1,13 +1,26 @@
+using System.Data.Common;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     public bool Open {get=>_open; set{_open=value; animator.SetBool("Open", value);}}
     [SerializeField] Animator animator;
     [SerializeField] bool _open;
+    [SerializeField] int sceneIndex; //Buildindex of the scene that loads when entering the door
+    [SerializeField] BoxCollider2D doorCollider;
+    [SerializeField] LayerMask playerLayer;
 
     void OnValidate()
     {
         animator.SetBool("Open", _open);
+    }
+    void Update()
+    {
+        if (doorCollider.IsTouchingLayers(playerLayer) && _open)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 }
